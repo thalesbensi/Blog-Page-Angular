2e8f66beb04dccabe5d0fb5e941b9bd7801c8022
thalesbensi <thalesbensi@outlook.com>
2025-04-04 14:56:42 -0300

    style: Update and Reorganize app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { TitleComponent } from './components/title/title.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SideSectionComponent } from './components/side-section/side-section.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, TitleComponent],
+  imports: [RouterOutlet, NavbarComponent, TitleComponent, BigCardComponent,SideSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
