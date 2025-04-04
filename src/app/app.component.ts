a4f9f9c7b826a496de3838ce7a834d111f04d79d
thalesbensi <thalesbensi@outlook.com>
2025-04-04 13:11:15 -0300

    fix: Update some project configs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TitleComponent } from './components/title/title.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, TitleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
